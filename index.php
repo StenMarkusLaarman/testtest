0b810ba75428ee83426b20001dc414ff87fff1b6
StenMarkusLaarman <115694132+StenMarkusLaarman@users.noreply.github.com>
2023-10-16 11:39:28 +0300

    Update index.php
    proov1

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-
+<?php	
+	$serverName = "tcp:yourserver.database.windows.net,1433";
+    $connectionOptions = array("Database"=>"yourdatabase",
+        "Uid"=>"yourusername", "PWD"=>"yourpassword");
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+?>
