1a28f952e349a7a78722420ea61092f3cceebbeb
StenMarkusLaarman <115694132+StenMarkusLaarman@users.noreply.github.com>
2023-10-16 10:29:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-php
 <!DOCTYPE html>
 <html>
 <head>
     <title>My Website</title>
 </head>
 <body>
-    <?php
-        echo "Hi there";
-    ?>
+    <h1>Hello there</h1>
+    <img src="https://example.com/starwars-meme.jpg" alt="Star Wars Meme">
 </body>
 </html>
